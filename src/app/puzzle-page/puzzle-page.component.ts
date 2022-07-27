bdec100f97b9521219cdcbed73a45d3be8382da6
João Pedro <joaopedro@catalyst.net.nz>
2022-07-27 17:11:46 +1200

    Don't show instructions again

--- src/app/puzzle-page/puzzle-page.component.ts
+++ src/app/puzzle-page/puzzle-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
 import { InstructionsModalComponent } from '../instructions-modal/instructions-modal.component';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-puzzle-page',
@@ -13,14 +14,16 @@ export class PuzzlePageComponent implements OnInit {
   token: string | null = null;
   puzzleId: string | null = null;
 
-  constructor(public route: ActivatedRoute, public dialog: MatDialog) { }
+  constructor(public route: ActivatedRoute, public dialog: MatDialog, public localStorageService: LocalStorageService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
       this.token = paramMap.get('token');
       this.puzzleId = paramMap.get('puzzleId');
     });
-    this.showInstructions();
+    if (!this.localStorageService.hasSeenInstructions) {
+      this.showInstructions();
+    }
   }
 
   showInstructions() {
--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,9 @@ const prefix = '/api/'
 export const HOST = 'https://api-degreezle.joopeeds.com'
 
 export const ENDPOINTS = {
-    puzzle: prefix + 'puzzle/', 
-    movie: prefix + 'movie/', 
-    person: prefix + 'person/', 
-    solution: prefix + 'solution/', 
-    metrics: prefix + 'metrics/'
+  puzzle: prefix + 'puzzle/',
+  movie: prefix + 'movie/',
+  person: prefix + 'person/',
+  solution: prefix + 'solution/',
+  metrics: prefix + 'metrics/'
 }
